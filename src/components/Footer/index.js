fe7a294893786d58983969af9280e80bdc0df1df
Muhammed Apak <muhampak25@gmail.com>
2023-07-12 21:27:30 +0300

    Footer text color changed

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,18 +1,36 @@
-import React from 'react';
-import { Box, Text, Link } from '@chakra-ui/react';
+import React from "react";
+import { Box, Text, Link } from "@chakra-ui/react";
 
 function Footer() {
   return (
-    <Box bg="gray.900" display={"flex"} justifyContent={"space-between"} color="white" py={4}  mt={15} position={"fixed"} bottom={0} left={0} right={0} px={10}>
-      <Text fontSize="sm" mb={2}>
-        &copy; {new Date().getFullYear()} Bizim Shop All rights reserved
-      </Text>
-      <Text fontSize="sm" mb={2}>
+    <Box
+      bg="gray.900"
+      display={{ base: 'block', md: 'flex' }}
+      justifyContent={"space-between"}
+      color="white"
+      py={4}
+      mt={15}
+      position={"fixed"}
+      bottom={0}
+      left={0}
+      right={0}
+      px={10}
+      textAlign={{base:"center"}}
+    >
+      <Text fontSize="sm" mb={2} bgClip={"text"} bgGradient={"linear(to right, #005aa7, #fffde4)"} >
         Development by Muhammed Apak
       </Text>
-      <Text fontSize="sm">
-        Source code available on{' '}
-        <Link href="https://github.com/apak0/Bizim-Shop" color="blue.300" isExternal>
+      <Text fontSize="md" mb={2}  bgClip='text'  bgGradient={"linear(to right, #12c2e9, #c471ed, #f64f59)"}>
+        &copy; {new Date().getFullYear()} Bizim Shop All rights reserved
+      </Text>
+
+      <Text fontSize="sm" bgClip={"text"} bgGradient={"linear(to right,  #7303c0, #ec38bc, #fdeff9);"}>
+        Source code available on{" "}
+        <Link
+          href="https://github.com/apak0/Bizim-Shop"
+          color="blue.300"
+          isExternal
+        >
           GitHub
         </Link>
       </Text>
